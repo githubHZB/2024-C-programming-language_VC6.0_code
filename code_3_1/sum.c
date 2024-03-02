fc7ae6685c002a0b9d8ca1e78ae869bf21ba5b9c
githubHZB <156395304+githubHZB@users.noreply.github.com>
2024-03-02 12:19:03 +0800

    C语言程序设计练习代码

---
+++ code_3_1/sum.c
@@ -0,0 +1,16 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a,b,c,max;
+	printf("please input a,b,c:\n");
+	scanf("%d%d%d",&a,&b,&c);
+    max=a;
+	if(max<b)
+	max=b;
+	if(max<c)
+    max=c;
+	printf("The Iargest numder is %d\n", max);
+	return 0;
+}
+
